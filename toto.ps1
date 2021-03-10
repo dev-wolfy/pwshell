238dcc3314cc7a7de6934da516801caa59e34fe7
wolfgit53 <jeanbaptiste.louze@gmail.com>
2021-03-10 10:22:25 +0100

    Update toto.ps1

--- toto.ps1
+++ toto.ps1
@@ -3,3 +3,11 @@ $listeusers = get-aduser -filter * -Properties department,title -SearchBase "OU=
 ($listeusers | group department,title).name | foreach-object{
     new-adgroup -GroupScope Global "GG_$($_.split(",")[0].replace('/','').trim())_$($_.split(",")[1].trim())"
 }
+
+# Ajouter les utilisateurs dans les groupes de sécurité GG
+
+$listeusers = get-aduser -filter * -Properties department,title -SearchBase "OU=_services,DC=$env:userdomain,DC=local"
+($listeusers | group department,title) | %{
+    Add-ADGroupMember -Identity "GG_$($_.name.split(",")[0].replace('/','').trim())_$($_.name.split(",")[1].trim())" `
+    -members $_.group
+}
